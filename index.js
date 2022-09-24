ad7907d01520862f316fcce6cd0f074f458ff1b0
Oksana1892 <Sidorkurs@yandex.ru>
2022-09-24 10:46:20 +0300

    первый файл

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log ('Hello world');
+
+const ourPlanetName = 'Земля';
+console.log(ourPlanetName);
+
+let currentTime = '12:12';
+console.log(currentTime);
+
+let articlesCount = 8;
+console.log(articlesCount);
+
+let isCash = 'no';
+let isCard = 'yes';
+console.log(isCash);
+
+const firstName = 'Горбунова';
+const secondName = 'Оксана';
+const thirdName = 'Владимировна';
+console.log(firstName, secondName, thirdName);
